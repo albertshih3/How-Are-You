3a08f5663d243facc99e1f01f29abd269f897971
Albert Shih <albertshih3@gmail.com>
2024-03-16 17:43:35 -0700

    Removed boilerplate code & add Firebase
    I removed the pregenerated code and replaced it with some extremely basic code. Also added back in firebase files and init.
    
    Signed-off-by: Albert Shih <albertshih3@gmail.com>

---
+++ src/app/components/navigation/navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'; 
+
+export default function Navbar() {
+    return (
+        <nav className = 'navbar'>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href='/login'>Login</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
